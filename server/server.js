c78b59cea34682bb6879eb7fd712a227fc021664
arfidakai <arfidapinata2703@gmail.com>
2025-10-16 18:44:32 +0700

    deploy

--- server/server.js
+++ server/server.js
@@ -54,6 +54,7 @@ const PORT = process.env.PORT || 3000;
 if (process.env.NODE_ENV !== "production") {
   dotenv.config();
 }
-app.listen(PORT, () =>
-  console.log(`🚀 Server running on http://localhost:${PORT}`)
+
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`)
 );
